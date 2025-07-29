02804e5dc7ee9c8f294c48d4b65cf376ff0d633c
Siddhant Lad <siddhantnlad@gmail.com>
2025-07-29 13:06:48 -0700

    Fixes

---
+++ src/pages/Index.tsx
@@ -0,0 +1,17 @@
+import HeroSection from "@/components/HeroSection";
+import FeaturesSection from "@/components/FeaturesSection";
+import EarlyAccessSection from "@/components/EarlyAccessSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <HeroSection />
+      <FeaturesSection />
+      <EarlyAccessSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
